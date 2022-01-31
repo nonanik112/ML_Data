c498c2f0cb14d38e686826dc058d1a2bc3563ec5
nonanik112 <metehan_1306@hotmail.com>
2022-01-31 23:34:27 +0300

    Data_ML

---
+++ ML_Data/ItemStock.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace ML_Data
+{
+    public class ItemStock
+    {
+        [Column("0")]
+        public string ItemID;
+
+        [Column("1")]
+        public float Loccode;
+
+        [Column("2")]
+        public float InQty;
+
+        [Column("3")]
+        public float OutQty;
+
+        [Column("4")]
+        public string ItemType;
+
+        [Column("5")]
+        public float TotalStockQty;
+    }
+    public class itemStockQtyPrediction
+    {
+        [Column("Score")]
+        public float TotalStockQty;
+    }
+}
